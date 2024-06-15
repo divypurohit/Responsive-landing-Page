93d18eee80b4a6a7bf756ec86a0daba173a41004
divypurohit <144053629+divypurohit@users.noreply.github.com>
2024-06-15 18:45:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = document.querySelector('.js');
+console.log(a);
+var flag =0;
+var b = document.querySelector('.dropdownMenu');
+b.addEventListener("click" , function(){
+    if(flag==0){
+        console.log("hey")
+    }
+})
+
